1a37d368be89e0d4a48a9ffe21afb8d229e29f17
BM-a81824 <55413298+BM-a81824@users.noreply.github.com>
2019-12-23 13:26:11 +0000

    ola

--- project/settings.py
+++ project/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'teste',
-        'USER': 'Marcelo',
+        'USER': 'Master',
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '5432',
