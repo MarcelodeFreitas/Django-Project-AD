e65699fbcbd6c482b61f5577109ad79f894b8afc
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2019-12-27 09:39:57 +0000

    lots of stuff

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,7 +6,7 @@ app_name = 'webapp'
 urlpatterns = [
     path('auth/', views.auth),
     path('home/', views.home, name='home'),
-    path('add_appuser/', views.add_appuser_view),
+    path('home/add_appuser/', views.add_appuser_view),
     path('register/', views.register, name='register'),
 ]
 
